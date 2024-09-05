21fb78f2edfc4aa0a01233a56f84110f5b0aa06d
Alejandra K. Justiniano <kiaraalejandrajustinianoolmos@gmail.com>
2024-09-05 01:45:04 -0300

    modificaciones de detalles

---
+++ proyecto-a-importar/App/Intento numero 15/static/js/overlay.js
@@ -0,0 +1,7 @@
+function openOverlay() {
+    document.getElementById('upload-overlay').style.display = 'flex';
+}
+
+function closeOverlay() {
+    document.getElementById('upload-overlay').style.display = 'none';
+}
